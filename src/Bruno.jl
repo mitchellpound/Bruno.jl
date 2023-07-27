4f977c70327c5fae04dab82b52b16d4a6557dc1b
mitchellpound <mitchell.pound@usu.edu>
2023-07-27 16:17:09 -0600

    Added ! to destructive fuctions

--- src/Bruno.jl
+++ src/Bruno.jl
@@ -64,7 +64,7 @@ export find_correlation_coeff, strategy_returns, strategy
 # new ones... just for now...
 export PriceType, StaticPrice, HistoricPrices, HistoricTrait, IsHistoric, NotHistoric, checkhistoric
 export price_vec, underlying, price_history, volatility_history, timesteps_per_period, BaseAsset, Derivative
-export SimulationEnvironment, add_asset, add_variable, add_interest_rate
+export SimulationEnvironment, add_asset!, add_variable!, add_interest_rate!
 export SimVariable, get_type
 
 export time_lag_price
