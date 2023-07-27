e2f679711aecf797dc87b9565aebb55415bc421a
socs22 <soclemens@gmail.com>
2023-07-27 18:34:15 -0400

    Added a few basic tests for Derivitive tests

--- test/simenvtests.jl
+++ test/simenvtests.jl
@@ -39,15 +39,17 @@
 
         @testset verbose=true "Derivitive tests" begin
             m = SimulationEnvironment(10, 252, 5, 100.0)
+            sim_to = 15
+            strike_price = 10
             hist_prices = Float64[1:5...]
-            future_prices = Float64[6:15...]
-            add_asset(m, EuroCallOption, hist_prices, future_prices, "test")
-            add_asset(m, Stock, hist_prices, future_prices, "test_2", 5)
-            @test m["test"] == Float64[1:15...]
-            @test m["test_2"] == Float64[1:15...]
-            @test m.starting_holdings["test"] == 0
-            @test m.starting_holdings["test_2"] == 5
-            @test get_type(m, "test") == Stock 
+            future_prices = Float64[6:sim_to...]
+            add_asset(m, Stock, hist_prices, future_prices, "test", 5)
+
+            add_asset(m, EuroCallOption, BlackScholes, "test", strike_price, 1, "der_test", 1)
+            
+            @test all(y->ismissing(y), m["der_test"][1:4])  # Check if the first 4 values are missing as they cant exist due to math constraints
+            @test length(m["der_test"][5]) == 10  # The lag prices should be 10 long as that is the 
+            @test isapprox(m["der_test"][end][end], sim_to - strike_price, atol=0.001)
     
             #test that volititly is working...
             end
